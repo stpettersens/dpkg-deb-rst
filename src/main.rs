493f9df05501cc36344693207445593c67bc06c6
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-09-21 19:00:36 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,30 @@ mod dpkgdeb;
 extern crate clioptions;
 use clioptions::CliOptions;
 
+fn display_error(program: &str, err: &str) {
+    println!("Error: {}.", err);
+    display_usage(&program, 2);
+}
+
+fn display_version() {
+
+}
+
+fn display_usage(program: &str, code: i32) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CliOptions::new("dpkg-deb-rst");
+    let program = cli.get_program();
+
+    if cli.get_num() > 1 {
+        for (i, a) in cli.get_args().iter().enumerate() {
+            match a.trim() {
+                "-h" | "--help" => display_usage(&program, 0),
+                "-v" | "--version" => display_version(),
+                _ => continue,
+            }
+        }
+    }
 }
